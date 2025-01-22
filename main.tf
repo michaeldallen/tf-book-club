847dac8f968f5f4a6f7f0dc124d1cd134726343d
MichaelDAllen <michaeldallen>
2025-01-22 00:51:39 +0000

    feat: single web server

--- main.tf
+++ main.tf
@@ -3,13 +3,35 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "example" {
+resource "aws_instance" "helloword-instance" {
   ami           = "ami-0df8c184d5f6ae949"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "mallen-tf-book-club-chapter2"
+    Name = "mallen-tf-book-club-chapter2-instance"
   }
 
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "biteme" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+
+  user_data_replace_on_change = true
+
+vpc_security_group_ids = [aws_security_group.helloword-security-group.id]
 }
 
+
+
+resource "aws_security_group" "helloword-security-group" {
+
+  name = "mallen-tf-book-club-chapter2-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
