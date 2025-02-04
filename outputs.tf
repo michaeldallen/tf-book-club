513fe6981cbba2e3adbf87887922608e08fd5bd6
MichaelDAllen <michaeldallen>
2025-02-04 23:51:21 +0000

    closer, but still sucking

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "public_ip" {
   description = "public IP of book club server"
   value       = aws_instance.hello-world-instance.public_ip
-    description = "The public IP address of the Hello World instance"
 
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "server_port" {
   description = "non-privileged port for HTTP server"
   type        = number
-  default     = 8080
+  default     = 80
 }
 
 variable "aws_region" {
   description = "The AWS region to create resources in"
   default     = "us-east-1"
 }
-
