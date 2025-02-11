ebeb2ac5382a75d4468387033f0aa8adad53b8f1
MichaelDAllen <michaeldallen>
2025-02-11 20:17:17 +0000

    set up shared variables and providers for aws

---
+++ exercises/aws/variables-aws.tf
@@ -0,0 +1,4 @@
+variable "aws_region" {
+  description = "The AWS region to host Hello, World"
+  default     = "us-east-1"
+}
--- exercises/variables.tf
+++ exercises/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to host Hello, World"
-  default     = "us-east-1"
-}
-
 variable "server_port" {
   description = "port for receiving http requests"
   type        = number
