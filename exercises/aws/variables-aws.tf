425ecdccdfc39c03991b9d90ee0a770890285c48
MichaelDAllen <michaeldallen>
2025-02-11 20:42:31 +0000

    share variables and outputs

--- exercises/aws/variables-aws.tf
+++ exercises/aws/variables-aws.tf
@@ -2,3 +2,9 @@ variable "aws_region" {
   description = "The AWS region to host Hello, World"
   default     = "us-east-1"
 }
+
+variable "backend_storage_name" {
+  description = "Name of AWS S3 bucket for TF state storage"
+  default     = "tf-book-club-state"
+}
+
---
+++ exercises/backend/outputs-backend.tf
@@ -0,0 +1,4 @@
+output "backend_storage_name" {
+  value = var.backend_storage_name
+}
+
