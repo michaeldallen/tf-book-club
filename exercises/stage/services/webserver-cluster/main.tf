87030e80fea33a257cf642e27dd3de8e6eadc8a4
MichaelDAllen <michaeldallen>
2025-02-13 00:43:56 +0000

    stage using webserver cluster module

--- exercises/stage/services/webserver-cluster/main.tf
+++ exercises/stage/services/webserver-cluster/main.tf
@@ -1,9 +1,9 @@
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webservers-stage"
+  cluster_name           = "webservers-stage"
   db_remote_state_bucket = "tfbc-state"
-  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
 }
 
